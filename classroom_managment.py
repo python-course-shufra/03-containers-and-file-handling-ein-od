e66f13df57582e9f4614f8c8a513f2dcd9d63f0c
ein-od <brot11205@gmail.com>
2024-01-11 00:08:08 +0200

    hw

--- classroom_managment.py
+++ classroom_managment.py
@@ -42,31 +42,58 @@ def add_student(name, email=None):
              in lowercase, you can use the `s.lower()` method
     'grade': initialize with empty list
     """
-    pass
+    if email == None:
+        email = name.lower()+'@example.com'
+
+    student = {}
+    student['name'] = name
+    student['email'] = email
+    student['grade'] = []
+    classroom.append( student )
+
+
+def get_index(name):
+    index = -1
+    for item in classroom:
+        index += 1
+        if item.get('name') == name:
+            return index
 
 
 def delete_student(name):
     """Delete a student from the classroom"""
-    pass
+    del classroom[get_index(name)]
 
 
 def set_email(name, email):
     """Sets the email of the student"""
-    pass
+    classroom[get_index(name)]['email'] = email
 
 
 def add_grade(name, profession, grade):
     """Adds a new grade to the student grades"""
-    pass
-
+    classroom[get_index(name)]['grades'].append((profession, grade))
 
 def avg_grade(name, profession):
     """Returns the average of grades of the student
     in the specified profession
     """
-    pass
 
+    count_grades_of_profession = 0
+    sum_grades_of_profession = 0
+    student_grades = classroom[get_index(name)]['grades']
+    for grade in student_grades:
+        if grade[0] == profession:
+            count_grades_of_profession += 1
+            sum_grades_of_profession += grade[1]
+    return sum_grades_of_profession / count_grades_of_profession 
 
 def get_professions(name):
     """Returns a list of unique professions that student has grades in"""
-    pass
+
+    student_grades = classroom[get_index(name)]['grades']
+    student_professions = set()
+    for grade in student_grades:
+        student_professions.add(grade[0])
+    return list(student_professions)
+print(get_professions('Charlie'))
